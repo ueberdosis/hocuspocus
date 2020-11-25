7b55e9a4b5bfe9b674434262d6f4705eb62953cc
kriskbx <m@kris.cool>
2020-11-25 12:52:10 +0100

    refactor, add docblocks

--- src/TiptapCollaborationServer.js
+++ src/TiptapCollaborationServer.js
@@ -1,30 +1,38 @@
-import { createServer } from 'http'
 import WebSocket from 'ws'
-import { setupWSConnection } from './bin/utils.js'
+import {createServer} from 'http'
+import {setupWSConnection} from './bin/utils.js'
 
 class Server {
 
   configuration = {
     port: 8080,
+    debounce: true,
   }
 
-  server = null
+  httpServer = null
   websocketServer = null
 
+  /**
+   * Initialize
+   */
   constructor() {
-    this.server = createServer((request, response) => {
+    this.httpServer = createServer((request, response) => {
       response.writeHead(200, {'Content-Type': 'text/plain'})
       response.end('OK')
     })
 
-    this.websocketServer = new WebSocket.Server({server: this.server})
-    this.websocketServer.on('connection', (connection, request) => {
-      this.log(`New connection to ${request.url}`)
-
-      return setupWSConnection(connection, request)
+    this.websocketServer = new WebSocket.Server({
+      server: this.httpServer
     })
+
+    this.websocketServer.on('connection', this._handleConnection.bind(this))
   }
 
+  /**
+   * Configure the server
+   * @param configuration
+   * @returns {Server}
+   */
   create(configuration) {
     this.configuration = {
       ...this.configuration,
@@ -34,13 +42,32 @@ class Server {
     return this
   }
 
+  /**
+   * Start the server
+   */
   listen() {
-    this.server.listen(this.configuration.port, () => {
-      this.log(`Listening on: ${this.configuration.port}`)
+    this.httpServer.listen(this.configuration.port, () => {
+      this._log(`Listening on: ${this.configuration.port}`)
     })
   }
 
-  log(message) {
+  /**
+   * Handle an incoming connection request
+   * @param connection
+   * @param request
+   * @private
+   */
+  _handleConnection(connection, request) {
+    this._log(`New connection to ${request.url}`)
+
+    return setupWSConnection(connection, request)
+  }
+
+  /**
+   * Log messages to console
+   * @param message
+   */
+  _log(message) {
     console.log('\x1b[32m%s\x1b[0m', '🚀 [TiptapCollaborationServer]', message)
   }
 }
