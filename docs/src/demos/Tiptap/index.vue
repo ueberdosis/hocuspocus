d8a1400d7b1e1ec9bb5dfb26e565705cf2047e21
Hans Pagel <hans.pagel@ueber.io>
2020-12-17 14:51:10 +0100

    clean up

--- docs/src/demos/Tiptap/index.vue
+++ docs/src/demos/Tiptap/index.vue
@@ -242,30 +242,6 @@ export default {
   }
 }
 
-// .collaboration-status {
-//   border-radius: 5px;
-//   margin-top: 1rem;
-//   color: #616161;
-
-//   &::before {
-//     content: ' ';
-//     display: inline-block;
-//     width: 0.5rem;
-//     height: 0.5rem;
-//     background: #ccc;
-//     border-radius: 50%;
-//     margin-right: 0.5rem;
-//   }
-
-//   &--connecting::before {
-//     background: #fd9170;
-//   }
-
-//   &--connected::before {
-//     background: #9DEF8F;
-//   }
-// }
-
 /* Give a remote user a caret */
 .collaboration-cursor__caret {
   position: relative;
