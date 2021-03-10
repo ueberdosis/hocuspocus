7e83bcc3e0914f433a080f1164778f0419112226
kriskbx <m@kris.cool>
2021-03-10 11:03:41 +0100

    improve stuff

--- packages/monitor/dashboard/tailwind.config.js
+++ packages/monitor/dashboard/tailwind.config.js
@@ -1,39 +1,14 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-  experimental: {},
-  purge: {
-    // content: ['./src/index.html', './src/**/*.vue'],
-  },
+  purge: [
+    './src/**/*.vue',
+    './src/*.html',
+  ],
   theme: {
-    interFontFeatures: {
-      default: ['calt', 'liga', 'kern'],
-      numeric: ['tnum', 'salt', 'ss02'],
-    },
-    extend: {
-      colors: {
-        gray: {
-          100: '#f5f5f5',
-          200: '#eeeeee',
-          300: '#e0e0e0',
-          400: '#bdbdbd',
-          500: '#9e9e9e',
-          600: '#757575',
-          700: '#616161',
-          800: '#424242',
-          900: '#212121',
-        },
-      },
-    },
+    extend: {},
   },
-  variants: {},
+  variants: {
+  },
   plugins: [
-    require('tailwindcss-font-inter')({
-      importFontFace: true,
-      disableUnusedFeatures: true,
-    }),
+    require('tailwindcss-font-inter')(),
   ],
 }
