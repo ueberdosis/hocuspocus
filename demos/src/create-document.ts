86c1b1bb39740f012737044695a15444180da479
kriskbx <m@kris.cool>
2021-03-18 12:17:01 +0100

    adjust demo

--- demos/src/create-document.ts
+++ demos/src/create-document.ts
@@ -4,32 +4,24 @@ import { Paragraph } from '@tiptap/extension-paragraph'
 import { Text } from '@tiptap/extension-text'
 import { prosemirrorJSONToYDoc } from 'y-prosemirror'
 
+import { Doc } from 'yjs'
 import { Logger } from '../../packages/logger/src'
 import { Server, onCreateDocumentPayload } from '../../packages/server/src'
 
 const server = Server.configure({
   port: 1234,
+  throttle: false,
 
   async onCreateDocument(data: onCreateDocumentPayload) {
-    const prosemirrorDocument = {
-      type: 'doc',
-      content: [
-        {
-          type: 'paragraph',
-          content: [
-            {
-              type: 'text',
-              text: 'Example Text',
-            },
-          ],
-        },
-      ],
-    }
+    const prosemirrorDocument = JSON.parse(`{
+      "type": "doc",
+      "content": [{ "type": "paragraph", "content": [{ "type": "text", "text": "Hello World!" }] }]
+    }`)
 
     const schema = getSchema([Document, Paragraph, Text])
     const ydoc = prosemirrorJSONToYDoc(schema, prosemirrorDocument, 'default')
 
-    console.log('onCreateDocument', ydoc.toJSON())
+    console.log('onCreateDocument:', ydoc.toJSON())
 
     return ydoc
   },
