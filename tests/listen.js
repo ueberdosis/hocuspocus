3ec584b80fd0d98914b9ec1a6acf2aaf0a8aff88
Hans Pagel <hans.pagel@ueber.io>
2021-05-21 15:04:07 +0200

    testing: refactor test

--- tests/listen.js
+++ tests/listen.js
@@ -2,43 +2,36 @@ import { chromium } from 'playwright'
 import assert from 'assert'
 import { Server } from '../packages/server/src'
 
-/**
- * Set up the server
- */
-const server = Server.configure({
-  port: 1234,
-})
-
-/**
- * Tests
- */
 context('listen', () => {
   let browser
-
   before(async () => {
     browser = await chromium.launch()
-
-    server.listen()
   })
-
   after(async () => {
     await browser.close()
-
-    server.destroy()
+    Server.destroy()
   })
 
   let page
-
   beforeEach(async () => {
     page = await browser.newPage()
   })
-
   afterEach(async () => {
     await page.close()
   })
 
-  it('should respond with OK', async () => {
-    await page.goto('http://localhost:1234/')
+  it('should respond with OK on the default port', async () => {
+    Server.listen()
+
+    await page.goto('http://localhost:80')
+
+    assert.strictEqual(await page.textContent('html'), 'OK')
+  })
+
+  it('should respond with OK on a custom port', async () => {
+    Server.configure({ port: 1234 }).listen()
+
+    await page.goto('http://localhost:1234')
 
     assert.strictEqual(await page.textContent('html'), 'OK')
   })
