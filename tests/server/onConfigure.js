5478b66456fda9eb34ad5d7ca8406548888cb227
Hans Pagel <hans.pagel@ueber.io>
2021-05-26 23:46:53 +0200

    add another test

---
+++ tests/server/onConfigure.js
@@ -0,0 +1,34 @@
+import assert from 'assert'
+import * as Y from 'yjs'
+import WebSocket from 'ws'
+import { Hocuspocus } from '../../packages/server/src'
+import { HocuspocusClient } from '../../packages/client/src'
+
+let client
+const ydoc = new Y.Doc()
+const Server = new Hocuspocus()
+
+context('server/onConfigure', () => {
+  afterEach(() => {
+    Server.destroy()
+    client.destroy()
+  })
+
+  it('onConfigure callback is executed', done => {
+    Server.configure({
+      port: 4000,
+      async onConfigure() {
+        setTimeout(() => {
+          done()
+        }, 0)
+      },
+    }).listen()
+
+    client = new HocuspocusClient({
+      url: 'ws://127.0.0.1:4000',
+      name: 'hocuspocus-demo',
+      document: ydoc,
+      WebSocketPolyfill: WebSocket,
+    })
+  })
+})
