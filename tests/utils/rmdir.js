714d3e1506f2a27955b9a7ba33dc164757ca3120
Hans Pagel <hans.pagel@ueber.io>
2021-05-28 16:49:54 +0200

    add basic rocksdb tests

---
+++ tests/utils/rmdir.js
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import path from 'path'
+
+export default dir => {
+  try {
+    const list = fs.readdirSync(dir)
+
+    for (let i = 0; i < list.length; i += 1) {
+      const filename = path.join(dir, list[i])
+      const stat = fs.statSync(filename)
+
+      if (filename === '.' || filename === '..') {
+        // pass these files
+      } else if (stat.isDirectory()) {
+        // rmdir recursively
+        rmdir(filename)
+      } else {
+        // rm fiilename
+        fs.unlinkSync(filename)
+      }
+    }
+
+    fs.rmdirSync(dir)
+  } catch {
+    //
+  }
+}
