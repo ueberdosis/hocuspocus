92803472a1439f73fc94695f0cfd5505b3377692
Hans Pagel <hans.pagel@ueber.io>
2021-07-13 18:15:58 +0200

    tests: test for async onCreateDocument hooks, see #132

--- tests/provider/onSynced.js
+++ tests/provider/onSynced.js
@@ -5,7 +5,6 @@ import { Hocuspocus } from '../../packages/server/src'
 import { HocuspocusProvider } from '../../packages/provider/src'
 
 let client
-const ydoc = new Y.Doc()
 
 context('provider/onSynced', () => {
   afterEach(() => {
@@ -13,6 +12,8 @@ context('provider/onSynced', () => {
   })
 
   it('onSynced callback is executed', done => {
+    const ydoc = new Y.Doc()
+
     const Server = new Hocuspocus()
     Server.configure({ port: 4000 }).listen()
 
@@ -30,6 +31,8 @@ context('provider/onSynced', () => {
   })
 
   it("on('synced') callback is executed", done => {
+    const ydoc = new Y.Doc()
+
     const Server = new Hocuspocus()
     Server.configure({ port: 4000 }).listen()
 
@@ -48,6 +51,8 @@ context('provider/onSynced', () => {
   })
 
   it('onSynced callback is executed, even when the onConnect takes longer', done => {
+    const ydoc = new Y.Doc()
+
     const Server = new Hocuspocus()
     Server.configure({
       port: 4000,
@@ -73,11 +78,46 @@ context('provider/onSynced', () => {
   })
 
   it('onSynced callback is executed when the document is actually synced', done => {
+    const ydoc = new Y.Doc()
+
     const Server = new Hocuspocus()
     Server.configure({
       port: 4000,
 
       async onCreateDocument({ document }) {
+        document.getArray('foo').insert(0, ['bar'])
+
+        return document
+      },
+    }).listen()
+
+    client = new HocuspocusProvider({
+      url: 'ws://127.0.0.1:4000',
+      name: 'hocuspocus-test',
+      document: ydoc,
+      WebSocketPolyfill: WebSocket,
+      onSynced: () => {
+        Server.destroy()
+
+        const value = ydoc.getArray('foo').get(0)
+        assert.strictEqual(value, 'bar')
+
+        done()
+      },
+    })
+  })
+
+  it('onSynced callback is executed when the document is actually synced, even if it takes longer', done => {
+    const ydoc = new Y.Doc()
+
+    const Server = new Hocuspocus()
+    Server.configure({
+      port: 4000,
+
+      async onCreateDocument({ document }) {
+        // sleep for 100ms
+        await new Promise(resolve => setTimeout(resolve, 100))
+
         document.getArray('foo').insert(0, ['bar'])
 
         return document
