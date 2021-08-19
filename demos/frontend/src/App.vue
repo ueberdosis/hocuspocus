460aa8c3b50cf66c9bf0644a4bd5d393d6ec0680
Hans Pagel <hans.pagel@ueber.io>
2021-08-19 17:10:13 +0200

    playground: improve styling

--- demos/frontend/src/App.vue
+++ demos/frontend/src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="flex flex-row">
-    <nav class="w-60 pt-4">
-      <ul>
-        <li><router-link class="block px-6 py-3" to="/">Text editing</router-link></li>
-        <li><router-link class="block px-6 py-3" to="/awareness">Awareness</router-link></li>
-        <li><router-link class="block px-6 py-3" to="/rooms">Rooms</router-link></li>
-        <li><router-link class="block px-6 py-3" to="/messages">Messages</router-link></li>
-        <li><router-link class="block px-6 py-3" to="/subdocuments">Subdocuments</router-link></li>
-      </ul>
-    </nav>
-    <main class="flex-grow mt-4 p-4">
-      <router-view />
-    </main>
+  <div>
+    <header class="border-b px-8 py-6 font-bold">
+      Playground
+    </header>
+    <div class="flex flex-row">
+      <nav class="w-60 pt-4 border-r">
+        <ul>
+          <li><router-link class="block px-8 py-3" to="/">Text editing</router-link></li>
+          <li><router-link class="block px-8 py-3" to="/awareness">Awareness</router-link></li>
+          <li><router-link class="block px-8 py-3" to="/rooms">Rooms</router-link></li>
+          <li><router-link class="block px-8 py-3" to="/messages">Messages</router-link></li>
+          <li><router-link class="block px-8 py-3" to="/subdocuments">Subdocuments</router-link></li>
+        </ul>
+      </nav>
+      <main class="flex-grow mt-4 px-8 py-4">
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
