60d9fab55d032f504bdb26a192c1e79316e33c96
Hans Pagel <hanspagel@users.noreply.github.com>
2021-08-20 17:50:53 +0200

    Use onAuthenticate hooks from custom extensions, see #170 (#172)
    * use onAuthenticate hooks from custom extensions
    
    * simplify the check for the onAuthenticate hook

--- packages/server/src/Hocuspocus.ts
+++ packages/server/src/Hocuspocus.ts
@@ -76,7 +76,9 @@ export class Hocuspocus {
   }
 
   get authenticationRequired(): boolean {
-    return this.configuration.onAuthenticate !== undefined
+    return !!this.configuration.extensions.find(extension => {
+      return extension.onAuthenticate !== undefined
+    })
   }
 
   /**
