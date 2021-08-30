a73b534f7e3eeb2dbe90005d1e64b128a62c7463
Hans Pagel <hans.pagel@ueber.io>
2021-08-30 13:52:55 +0200

    server: add all websocket ready states to the type

--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -12,7 +12,13 @@ export enum MessageType {
   Auth = 2,
 }
 
+/**
+ * State of the WebSocket connection.
+ * https://developer.mozilla.org/de/docs/Web/API/WebSocket/readyState
+ */
 export enum WsReadyStates {
+  Connecting = 0,
+  Open = 1,
   Closing = 2,
   Closed = 3,
 }
