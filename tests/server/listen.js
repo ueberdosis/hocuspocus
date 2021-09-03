497570efe4813c276d885dbd0f470c4ba02c28a9
Hans Pagel <hans.pagel@ueber.io>
2021-09-03 20:48:43 +0200

    tests: refactor server/listen test

--- tests/server/listen.js
+++ tests/server/listen.js
@@ -2,11 +2,7 @@ import { chromium } from 'playwright'
 import assert from 'assert'
 import { Hocuspocus } from '../../packages/server/src'
 
-const Server = new Hocuspocus()
-
-context('server/listen', function () {
-  this.timeout(10000)
-
+context('server/listen', () => {
   let browser
 
   before(async () => {
@@ -17,22 +13,15 @@ context('server/listen', function () {
     await browser.close()
   })
 
-  let page
-
-  beforeEach(async () => {
-    page = await browser.newPage()
-  })
-
-  afterEach(async () => {
-    await page.close()
-    Server.destroy()
-  })
-
   it('should respond with OK on a custom port', async () => {
+    const page = await browser.newPage()
+    const Server = new Hocuspocus()
     Server.configure({ port: 4000 }).listen()
 
     await page.goto('http://localhost:4000')
 
     assert.strictEqual(await page.textContent('html'), 'OK')
+    await page.close()
+    Server.destroy()
   })
 })
