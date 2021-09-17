544d0b5c6e7198c7c345e6ed3901c763910a2567
Hans Pagel <hans.pagel@ueber.io>
2021-09-17 14:43:49 +0200

    tests: test that nothing is sent if onAuthenticate fails

--- tests/server/onAuthenticate.js
+++ tests/server/onAuthenticate.js
@@ -60,6 +60,132 @@ context('server/onAuthenticate', () => {
     })
   })
 
+  it('doesn’t execute the onAuthenticate callback when no token is passed to the provider', done => {
+    const Server = new Hocuspocus()
+
+    Server.configure({
+      port: 4000,
+      async onAuthenticate() {
+        assert.fail()
+      },
+    }).listen()
+
+    client = new HocuspocusProvider({
+      url: 'ws://127.0.0.1:4000',
+      name: 'hocuspocus-test',
+      document: ydoc,
+      WebSocketPolyfill: WebSocket,
+      maxAttempts: 1,
+      // token: 'SUPER-SECRET-TOKEN',
+      onOpen: () => {
+        setTimeout(() => {
+          client.destroy()
+          Server.destroy()
+          done()
+        }, 100)
+      },
+    })
+  })
+
+  it('doesn’t send any message when no token is provided, but the onAuthenticate hook is configured', done => {
+    const Server = new Hocuspocus()
+
+    Server.configure({
+      port: 4000,
+      async onAuthenticate() {
+        assert.fail()
+      },
+    })
+    Server.enableDebugging()
+    Server.listen()
+
+    const client = new HocuspocusProvider({
+      url: 'ws://127.0.0.1:4000',
+      name: 'hocuspocus-test',
+      document: ydoc,
+      WebSocketPolyfill: WebSocket,
+      maxAttempts: 1,
+      onOpen: () => {
+        setTimeout(() => {
+          assert.deepStrictEqual(Server.getMessageLogs(), [])
+
+          client.destroy()
+          Server.destroy()
+
+          done()
+        }, 100)
+      },
+    })
+  })
+
+  it('confirms the `Token` message with an `Authenticated` message', done => {
+    const Server = new Hocuspocus()
+
+    Server.configure({
+      port: 4000,
+      async onAuthenticate() {
+        // success
+        return true
+      },
+    })
+    Server.enableDebugging()
+    Server.listen()
+
+    const client = new HocuspocusProvider({
+      url: 'ws://127.0.0.1:4000',
+      name: 'hocuspocus-test',
+      document: ydoc,
+      WebSocketPolyfill: WebSocket,
+      token: 'SUPER-SECRET-TOKEN',
+      maxAttempts: 1,
+      onAuthenticated: () => {
+        assert.deepStrictEqual(Server.getMessageLogs(), [
+          { category: 'Token', direction: 'in', type: 'Auth' },
+          { category: 'Authenticated', direction: 'out', type: 'Auth' },
+        ])
+
+        client.destroy()
+        Server.destroy()
+
+        done()
+      },
+    })
+  })
+
+  it('replies with a `PermissionDenied` message when authentication fails', done => {
+    const Server = new Hocuspocus()
+
+    Server.configure({
+      port: 4000,
+      async onAuthenticate() {
+        // fail
+        throw Error()
+      },
+    })
+    Server.enableDebugging()
+    Server.listen()
+
+    const client = new HocuspocusProvider({
+      url: 'ws://127.0.0.1:4000',
+      name: 'hocuspocus-test',
+      document: ydoc,
+      WebSocketPolyfill: WebSocket,
+      token: 'SUPER-SECRET-TOKEN',
+      maxAttempts: 1,
+      onAuthenticationFailed: () => {
+        assert.deepStrictEqual(Server.getMessageLogs(), [
+          { category: 'Token', direction: 'in', type: 'Auth' },
+          { category: 'PermissionDenied', direction: 'out', type: 'Auth' },
+        ])
+
+        client.destroy()
+        Server.destroy()
+
+        done()
+      },
+    })
+  })
+
   it('passes context from onAuthenticate to onCreateDocument', done => {
     const Server = new Hocuspocus()
 
