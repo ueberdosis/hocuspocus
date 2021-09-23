e869e30cb7b882f15a0178e803820337970c7357
Tom Moor <tom.moor@gmail.com>
2021-09-22 22:48:44 -0700

    refactor

--- packages/pubsub/src/index.ts
+++ packages/pubsub/src/index.ts
@@ -66,33 +66,7 @@ export class PubSub implements Extension {
     // debounced handler should be setup here in the constructor so that the
     // wait time can be configurable
     this.debouncedUpdate = debounce(
-      document => {
-        const ttl = 1000
-
-        // attempt to acquire a lock and read lastReceivedTimestamp from Redis,
-        // if the value < debounce start then it can call the onPersist callback
-        // for the host application to write to disk
-        this.redlock.lock(`${this.getKey(document.name)}:lock`, ttl, async (err, lock) => {
-          if (err || !lock) {
-            // could not acquire lock, expected behavior.
-            return
-          }
-
-          const result = await this.redis.get(`${this.getKey(document.name)}:updated`)
-          const updatedTime = result ? Date.parse(result) : undefined
-
-          if (updatedTime && updatedTime < Date.now() - this.configuration.persistWait) {
-            if (this.configuration.onPersist) {
-              this.configuration.onPersist(document)
-            }
-          }
-
-          lock.unlock(err => {
-            // we weren't able to reach redis; the lock will expire after ttl
-            console.error(err)
-          })
-        })
-      },
+      this.handlePersistDocument,
       this.configuration.persistWait,
     )
   }
@@ -133,60 +107,88 @@ export class PubSub implements Extension {
     })
   }
 
-   public onDisconnect = async ({ documentName, clientsCount }: onDisconnectPayload) => {
-     // Still clients connected?
-     if (clientsCount > 0) {
-       return
-     }
+  public onDisconnect = async ({ documentName, clientsCount }: onDisconnectPayload) => {
+    // Still clients connected?
+    if (clientsCount > 0) {
+      return
+    }
 
-     this.configuration.log('last disconnect', documentName)
+    this.configuration.log('last disconnect', documentName)
 
-     // TODO: persist first?
-     this.documents.delete(documentName)
+    // TODO: persist first?
+    this.documents.delete(documentName)
 
-     // on final connection close sub channel
-     this.sub.unsubscribe(this.getKey(documentName), err => {
-       if (err) {
-         console.error(err)
-         return
-       }
-       this.configuration.log(`Unsubscribed from ${documentName}`)
-     })
-   }
+    // on final connection close sub channel
+    this.sub.unsubscribe(this.getKey(documentName), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      this.configuration.log(`Unsubscribed from ${documentName}`)
+    })
+  }
 
-   /**
+  private handlePersistDocument = async (document: Document) => {
+    const ttl = 1000
+
+    // attempt to acquire a lock and read lastReceivedTimestamp from Redis,
+    // if the value < debounce start then it can call the onPersist callback
+    // for the host application to write to disk
+    this.redlock.lock(`${this.getKey(document.name)}:lock`, ttl, async (err, lock) => {
+      if (err || !lock) {
+        // could not acquire lock, expected behavior.
+        return
+      }
+
+      const result = await this.redis.get(`${this.getKey(document.name)}:updated`)
+      const updatedTime = result ? Date.parse(result) : undefined
+
+      if (updatedTime && updatedTime < Date.now() - this.configuration.persistWait) {
+        if (this.configuration.onPersist) {
+          this.configuration.onPersist(document)
+        }
+      }
+
+      lock.unlock(err => {
+        // we weren't able to reach redis; the lock will expire after ttl
+        console.error(err)
+      })
+    })
+  }
+
+  /**
    * Handle awareness update messages received directly by this hocuspocus instance.
   */
-   private handleAwarenessUpdate(document: Document) {
-     return async () => {
-       const message = new OutgoingMessage()
-         .createAwarenessUpdateMessage(document.awareness)
+  private handleAwarenessUpdate(document: Document) {
+    return async () => {
+      const message = new OutgoingMessage()
+        .createAwarenessUpdateMessage(document.awareness)
 
-       await this.redis.publishBuffer(this.getKey(document.name), Buffer.from(message.toUint8Array()))
-     }
-   }
+      await this.redis.publishBuffer(this.getKey(document.name), Buffer.from(message.toUint8Array()))
+    }
+  }
 
-   /**
+  /**
    * Handle document update messages received directly by this hocuspocus instance.
   */
-   private handleUpdate(document: Document) {
-     return async (update: Uint8Array) => {
-       this.configuration.log('publishing local update')
+  private handleUpdate(document: Document) {
+    return async (update: Uint8Array) => {
+      this.configuration.log('publishing local update')
 
-       const message = new OutgoingMessage()
-         .createSyncMessage()
-         .writeUpdate(update)
+      const message = new OutgoingMessage()
+        .createSyncMessage()
+        .writeUpdate(update)
 
-       // forward all update messages received to redis channel
-       await this.redis.publishBuffer(this.getKey(document.name), Buffer.from(message.toUint8Array()))
+      // forward all update messages received to redis channel
+      await this.redis.publishBuffer(this.getKey(document.name), Buffer.from(message.toUint8Array()))
 
-       // update the lastReceivedTimestamp in a Redis key for documentName if source is this server.
-       await this.redis.set(`${this.getKey(document.name)}:updated`, new Date().toISOString())
+      // update the lastReceivedTimestamp in a Redis key for documentName if source is this server.
+      await this.redis.set(`${this.getKey(document.name)}:updated`, new Date().toISOString())
 
-       // When a node receives an update event from the client or another server it sets an in memory debounce (eg 3 seconds)
-       this.debouncedUpdate(document)
-     }
-   }
+      // When a node receives an update event from the client or another server it sets an in memory debounce (eg 3 seconds)
+      this.debouncedUpdate(document)
+    }
+  }
 
   /**
    * Handle incoming messages published on all subscribed document channels.
