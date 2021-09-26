409a1445223624306d2a4ad8f63bfeb5bd223eb5
Tom Moor <tom.moor@gmail.com>
2021-09-25 19:53:19 -0700

    test: onDisconnect

---
+++ tests/pubsub/onDisconnect.js
@@ -0,0 +1,53 @@
+import assert from 'assert'
+import * as Y from 'yjs'
+import WebSocket from 'ws'
+import { Hocuspocus } from '../../packages/server/src'
+import { PubSub } from '../../packages/pubsub/src'
+import { HocuspocusProvider } from '../../packages/provider/src'
+
+const server = new Hocuspocus()
+const server1 = new Hocuspocus()
+
+const opts = {
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+}
+
+context('pubsub/onDisconnect', () => {
+  it('calls persist after last client disconnects', done => {
+    const ydoc = new Y.Doc()
+    let client
+
+    server.configure({
+      port: 4000,
+      extensions: [
+        new PubSub({
+          ...opts,
+          instanceName: 'server',
+          log: (...args) => console.log('server:', ...args),
+          onPersist: doc => {
+            assert.strictEqual(ydoc.getArray('foo').get(0), doc.getArray('foo').get(0))
+
+            client.destroy()
+            server.destroy()
+            done()
+          },
+        }),
+      ],
+    }).listen()
+
+    client = new HocuspocusProvider({
+      url: 'ws://127.0.0.1:4000',
+      name: 'hocuspocus-test',
+      document: ydoc,
+      WebSocketPolyfill: WebSocket,
+      maxAttempts: 1,
+      broadcast: false,
+    })
+
+    client.on('synced', () => {
+      ydoc.getArray('foo').insert(0, ['bar'])
+      client.disconnect()
+    })
+  }).timeout(5000)
+})
