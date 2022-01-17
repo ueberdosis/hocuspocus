78ef1d0db6b5051d5987c7e392b8c425a7a5e3ac
Hans Pagel <hans.pagel@ueber.io>
2022-01-17 16:23:33 +0100

    migrate another test

--- ava-tests/server/listen.ts
+++ ava-tests/server/listen.ts
@@ -1,28 +1,28 @@
-import test, { ExecutionContext } from 'ava'
-import { Page } from 'playwright'
+import test from 'ava'
 import { Hocuspocus } from '@hocuspocus/server'
-import { newHocuspocus, pageMacro } from '../utils'
+import fetch from 'node-fetch'
+import { newHocuspocus } from '../utils'
 
-test('should respond with OK', pageMacro, async (t: ExecutionContext, page: Page) => {
+test('should respond with OK', async t => {
   const server = await newHocuspocus()
 
-  await page.goto(server.httpURL)
+  const response = await fetch(server.httpURL)
 
-  t.is(await page.textContent('html'), 'OK')
+  t.is(await response.text(), 'OK')
 })
 
-test('should respond with OK on a custom port', pageMacro, async (t: ExecutionContext, page: Page) => {
+test('should respond with OK on a custom port', async t => {
   const server = await newHocuspocus({
     port: 4000,
   })
 
-  await page.goto(server.httpURL)
+  const response = await fetch(server.httpURL)
 
   t.is(server.address.port, 4000)
-  t.is(await page.textContent('html'), 'OK')
+  t.is(await response.text(), 'OK')
 })
 
-test('should respond with OK on a custom port passed to listen()', pageMacro, async (t: ExecutionContext, page: Page) => {
+test('should respond with OK on a custom port passed to listen()', async t => {
   const server = new Hocuspocus().configure({
     quiet: true,
     port: 0,
@@ -30,13 +30,13 @@ test('should respond with OK on a custom port passed to listen()', pageMacro, as
 
   server.listen(4001)
 
-  await page.goto(server.httpURL)
+  const response = await fetch(server.httpURL)
 
   t.is(server.address.port, 4001)
-  t.is(await page.textContent('html'), 'OK')
+  t.is(await response.text(), 'OK')
 })
 
-test('should take a custom port and a callback', pageMacro, async (t: ExecutionContext, page: Page) => {
+test('should take a custom port and a callback', async t => {
   const server = new Hocuspocus().configure({
     quiet: true,
     port: 0,
@@ -48,13 +48,13 @@ test('should take a custom port and a callback', pageMacro, async (t: ExecutionC
     })
   })
 
-  await page.goto(server.httpURL)
+  const response = await fetch(server.httpURL)
 
   t.is(server.address.port, 4002)
-  t.is(await page.textContent('html'), 'OK')
+  t.is(await response.text(), 'OK')
 })
 
-test('should execute a callback', pageMacro, async (t: ExecutionContext, page: Page) => {
+test('should execute a callback', async t => {
   const server = new Hocuspocus().configure({
     quiet: true,
     port: 0,
@@ -66,12 +66,12 @@ test('should execute a callback', pageMacro, async (t: ExecutionContext, page: P
     })
   })
 
-  await page.goto(server.httpURL)
+  const response = await fetch(server.httpURL)
 
-  t.is(await page.textContent('html'), 'OK')
+  t.is(await response.text(), 'OK')
 })
 
-test('should have the custom port as a parameter in the callback', pageMacro, async (t: ExecutionContext, page: Page) => {
+test('should have the custom port as a parameter in the callback', async t => {
   const server = new Hocuspocus().configure({
     quiet: true,
     port: 0,
@@ -84,7 +84,7 @@ test('should have the custom port as a parameter in the callback', pageMacro, as
     })
   })
 
-  await page.goto(server.httpURL)
+  const response = await fetch(server.httpURL)
 
-  t.is(await page.textContent('html'), 'OK')
+  t.is(await response.text(), 'OK')
 })
