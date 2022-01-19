957dc518d4b76aefa6f5394ed111a5eb47205157
Hans Pagel <hans.pagel@ueber.io>
2022-01-19 20:21:56 +0100

    migrate server/onLoadDocument

--- ava-tests/server/onStoreDocument.ts
+++ ava-tests/server/onStoreDocument.ts
@@ -1,6 +1,5 @@
 import test from 'ava'
-import { onRequestPayload, onStoreDocumentPayload } from '@hocuspocus/server'
-import fetch from 'node-fetch'
+import { onStoreDocumentPayload } from '@hocuspocus/server'
 import { HocuspocusProvider } from '@hocuspocus/provider'
 import { newHocuspocus, newHocuspocusProvider, sleep } from '../utils'
 
