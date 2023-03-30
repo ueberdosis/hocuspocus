fb537a3507903a16a248db971c8715f70e02ca2a
Jan Thurau <jan@janthurau.de>
2023-03-30 16:13:51 +0200

    Removes extension-monitor

--- packages/extension-monitor/dashboard/src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import VueVirtualScroller from 'vue-virtual-scroller'
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
-import App from './components/App.vue'
-
-Vue.use(VueVirtualScroller)
-
-new Vue({ render: createElement => createElement(App) }).$mount('#app')
