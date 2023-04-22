542119aa7c17cfa482c47c36b4c461182ce6033c
Jan Thurau <jan@janthurau.de>
2023-04-22 21:23:27 +0200

    fixes exports

--- packages/provider/src/index.ts
+++ packages/provider/src/index.ts
@@ -1,4 +1,5 @@
 export * from './HocuspocusProvider'
 export * from './TiptapCollabProvider'
+export * from './TiptapCollabProviderWebsocket'
 export * from './HocuspocusProviderWebsocket'
 export * from './types'
