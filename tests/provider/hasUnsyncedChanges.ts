711f5de609a6de628700fc1ff85bfae795e24984
yousefed <yousefdardiry@gmail.com>
2023-05-22 13:57:34 +0200

    add tests for hasUnsyncedChanges

---
+++ tests/provider/hasUnsyncedChanges.ts
@@ -0,0 +1,86 @@
+import test from 'ava'
+import { retryableAssertion } from 'tests/utils/retryableAssertion'
+import { newHocuspocus, newHocuspocusProvider, sleep } from '../utils'
+
+test("initially doesn't have unsynced changes", async t => {
+
+  const server = await newHocuspocus()
+
+  const provider = newHocuspocusProvider(server)
+
+  t.is(provider.hasUnsyncedChanges, false)
+
+})
+
+test('has unsynced changes when updating', async t => {
+
+  const server = await newHocuspocus()
+
+  const provider = newHocuspocusProvider(server)
+
+  provider.document.getMap('test').set('foo', 'bar')
+
+  await retryableAssertion(t, tt => {
+    tt.is(provider.hasUnsyncedChanges, true)
+  })
+
+  // changes are synced
+  await retryableAssertion(t, tt => {
+    tt.is(provider.hasUnsyncedChanges, false)
+  })
+})
+
+test('has unsynced changes when in readonly mode', async t => {
+
+  const server = await newHocuspocus({
+    async onAuthenticate({ connection }) {
+      connection.readOnly = true
+    },
+  })
+
+  const provider = newHocuspocusProvider(server)
+
+  provider.document.getMap('test').set('foo', 'bar')
+
+  await retryableAssertion(t, tt => {
+    tt.is(provider.hasUnsyncedChanges, true)
+  })
+
+  await sleep(100)
+
+  t.is(provider.hasUnsyncedChanges, true)
+})
+
+test('has unsynced changes when in readonly mode and receiving external update', async t => {
+
+  const server = await newHocuspocus({
+    async onAuthenticate({ connection, token }) {
+      if (token === 'readonly') {
+        connection.readOnly = true
+      }
+    },
+  })
+
+  const provider = newHocuspocusProvider(server, {
+    token: 'readonly',
+  })
+
+  provider.document.getMap('test').set('foo', 'bar')
+
+  await retryableAssertion(t, tt => {
+    tt.is(provider.hasUnsyncedChanges, true)
+  })
+
+  await sleep(100)
+
+  t.is(provider.hasUnsyncedChanges, true)
+
+  const provider2 = newHocuspocusProvider(server)
+
+  provider2.document.getMap('test2').set('foo', 'bar')
+
+  await sleep(100)
+
+  t.is(provider2.hasUnsyncedChanges, false)
+  t.is(provider.hasUnsyncedChanges, true) // TODO: this currently fails
+})
