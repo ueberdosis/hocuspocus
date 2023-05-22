d2578c275d662edb587d4f082100587adeb02a15
yousefed <yousefdardiry@gmail.com>
2023-05-22 16:53:12 +0200

    add test for unsynced changes with initial document

--- tests/provider/hasUnsyncedChanges.ts
+++ tests/provider/hasUnsyncedChanges.ts
@@ -1,5 +1,6 @@
 import test from 'ava'
 import { retryableAssertion } from 'tests/utils/retryableAssertion'
+import * as Y from 'yjs'
 import { newHocuspocus, newHocuspocusProvider, sleep } from '../utils'
 
 test("initially doesn't have unsynced changes", async t => {
@@ -41,6 +42,28 @@ test('has unsynced changes when in readonly mode', async t => {
   const provider = newHocuspocusProvider(server)
 
   provider.document.getMap('test').set('foo', 'bar')
+
+  await retryableAssertion(t, tt => {
+    tt.is(provider.hasUnsyncedChanges, true)
+  })
+
+  await sleep(100)
+
+  t.is(provider.hasUnsyncedChanges, true)
+})
+
+test.only('has unsynced changes when in readonly mode and initial document has changed', async t => {
+
+  const server = await newHocuspocus({
+    async onAuthenticate({ connection }) {
+      connection.readOnly = true
+    },
+  })
+
+  const document = new Y.Doc()
+  document.getMap('test').set('foo', 'bar')
+
+  const provider = newHocuspocusProvider(server, { document })
 
   await retryableAssertion(t, tt => {
     tt.is(provider.hasUnsyncedChanges, true)
