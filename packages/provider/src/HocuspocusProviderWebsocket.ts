797fc7cf0135d4b38b8a2df252760595df3de77b
groot9325 <120991021@qq.com>
2023-07-01 00:43:25 +0800

    fixes: HocuspocusProviderWebsocket checkConnection ineffective (#640)
    * fix: HocuspocusProviderWebsocket checkConnection ineffective
    
    * update lastMessageReceived in HocuspocusProviderWebsocket's onMessage

--- packages/provider/src/HocuspocusProviderWebsocket.ts
+++ packages/provider/src/HocuspocusProviderWebsocket.ts
@@ -314,6 +314,8 @@ export class HocuspocusProviderWebsocket extends EventEmitter {
 
   onMessage(event: MessageEvent) {
     this.resolveConnectionAttempt()
+
+    this.lastMessageReceived = time.getUnixTime()
   }
 
   resolveConnectionAttempt() {
