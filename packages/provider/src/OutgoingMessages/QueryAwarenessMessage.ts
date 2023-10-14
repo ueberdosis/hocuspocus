81f668016005ed7f648e2096efc1e0142b656648
Siddhartha <siddu.siddhartha@gmail.com>
2023-10-14 09:13:17 -0700

    Removing the console log messages (#726)
    * Removing the console log messages
    
    * Update QueryAwarenessMessage.ts
    
    * Removing trailing spaces

--- packages/provider/src/OutgoingMessages/QueryAwarenessMessage.ts
+++ packages/provider/src/OutgoingMessages/QueryAwarenessMessage.ts
@@ -9,9 +9,6 @@ export class QueryAwarenessMessage extends OutgoingMessage {
 
   get(args: Partial<OutgoingMessageArguments>) {
 
-    console.log('queryAwareness: writing string docName', args.documentName)
-    console.log(this.encoder.cpos)
-
     encoding.writeVarString(this.encoder, args.documentName!)
     encoding.writeVarUint(this.encoder, this.type)
 
