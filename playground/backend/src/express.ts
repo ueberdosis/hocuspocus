ddec7153e7d40725d6551d7dfbc61aac1b2b042e
Jan Thurau <jan@janthurau.de>
2025-05-06 08:37:12 +0200

    fix: fixes express/koa backend demo, fixes #939

--- playground/backend/src/express.ts
+++ playground/backend/src/express.ts
@@ -1,23 +1,21 @@
-import { Logger } from '@hocuspocus/extension-logger'
-import { Hocuspocus } from '@hocuspocus/server'
-import express from 'express'
-import expressWebsockets from 'express-ws'
+import { Logger } from "@hocuspocus/extension-logger";
+import { Hocuspocus } from "@hocuspocus/server";
+import express from "express";
+import expressWebsockets from "express-ws";
 
 const hocuspocus = new Hocuspocus({
-  extensions: [
-    new Logger(),
-  ],
-})
+	extensions: [new Logger()],
+});
 
-const { app } = expressWebsockets(express())
+const { app } = expressWebsockets(express());
 
-app.get('/', (request, response) => {
-  response.send('Hello World!')
-})
+app.get("/", (request, response) => {
+	response.send("Hello World!");
+});
 
-app.ws('/:documentName', (websocket, request: any) => {
-  const context = { user_id: 1234 }
-  hocuspocus.handleConnection(websocket, request, context)
-})
+app.ws("/", (websocket, request: any) => {
+	const context = { user_id: 1234 };
+	hocuspocus.handleConnection(websocket, request, context);
+});
 
-app.listen(1234, () => console.log('Listening on http://127.0.0.1:1234…'))
+app.listen(1234, () => console.log("Listening on http://127.0.0.1:1234…"));
