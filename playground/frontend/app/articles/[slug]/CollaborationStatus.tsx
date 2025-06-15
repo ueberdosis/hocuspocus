d20f0a03653d4de69081069fcf631a2e1ff8dc88
Jan Thurau <jan@janthurau.de>
2025-06-15 22:12:44 +0200

    chore: playground: fix status effect

--- playground/frontend/app/articles/[slug]/CollaborationStatus.tsx
+++ playground/frontend/app/articles/[slug]/CollaborationStatus.tsx
@@ -1,50 +1,85 @@
-import type {HocuspocusProvider, onUnsyncedChangesParameters} from "@hocuspocus/provider";
-import {useState, useEffect} from "react";
+import type {
+	HocuspocusProvider,
+	onStatusParameters,
+	onUnsyncedChangesParameters,
+} from "@hocuspocus/provider";
+import { useEffect, useState } from "react";
 
 const CollaborationStatus = (props: {
 	provider: HocuspocusProvider;
 }) => {
-  const { provider } = props;
+	const { provider } = props;
 
-  const [unsyncedChanges, setUnsyncedChanges] = useState(0);
+	const [unsyncedChanges, setUnsyncedChanges] = useState(0);
+	const [socketStatus, setSocketStatus] = useState<string | null>(null);
 
-  useEffect(() => {
-    const handleUnsyncedChanges = (changes: onUnsyncedChangesParameters) => {
-      setUnsyncedChanges(changes.number);
-    };
+	useEffect(() => {
+		setSocketStatus(provider.configuration.websocketProvider.status);
 
-    provider.on('unsyncedChanges', handleUnsyncedChanges);
+		const handleUnsyncedChanges = (changes: onUnsyncedChangesParameters) => {
+			setUnsyncedChanges(changes.number);
+		};
 
-    return () => {
-      provider.off('unsyncedChanges', handleUnsyncedChanges);
-    };
-  }, [provider]);
+		const handleSocketStatus = (status: onStatusParameters) => {
+			setSocketStatus(status.status);
+		};
 
-	return <div className="my-10">
-    <p>Socket status: {provider.configuration.websocketProvider.status}
-      / Provider status: {provider.isAttached ? 'attached' : 'detached'}
-    </p>
+		provider.on("unsyncedChanges", handleUnsyncedChanges);
+		provider.configuration.websocketProvider.on("status", handleSocketStatus);
 
-    <p>Unsynced changes: {unsyncedChanges}</p>
+		return () => {
+			provider.off("unsyncedChanges", handleUnsyncedChanges);
+			provider.configuration.websocketProvider.off(
+				"status",
+				handleSocketStatus,
+			);
+		};
+	}, [provider]);
 
-    <div className="flex flex-row gap-5 mt-3">
-      <div>
-        <p className="text-center">Socket</p>
-        <div className="flex flex-row gap-2">
-          <button className="border p-2" onClick={() => provider.configuration.websocketProvider.disconnect()}>Disconnect</button>
-          <button className="border p-2" onClick={() => provider.configuration.websocketProvider.connect()}>Connect</button>
-        </div>
-      </div>
+	return (
+		<div className="my-10">
+			<p>
+				Socket status: {socketStatus}/ Provider status:{" "}
+				{provider.isAttached ? "attached" : "detached"}
+			</p>
 
-      <div>
-        <p className="text-center">Provider</p>
-        <div className="flex flex-row gap-2">
-          <button className="border p-2" onClick={() => provider.detach()}>Detach</button>
-          <button className="border p-2" onClick={() => provider.attach()}>Attach</button>
-        </div>
-      </div>
-    </div>
-  </div>
+			<p>Unsynced changes: {unsyncedChanges}</p>
+
+			<div className="flex flex-row gap-5 mt-3">
+				<div>
+					<p className="text-center">Socket</p>
+					<div className="flex flex-row gap-2">
+						<button
+							className="border p-2"
+							onClick={() =>
+								provider.configuration.websocketProvider.disconnect()
+							}
+						>
+							Disconnect
+						</button>
+						<button
+							className="border p-2"
+							onClick={() => provider.configuration.websocketProvider.connect()}
+						>
+							Connect
+						</button>
+					</div>
+				</div>
+
+				<div>
+					<p className="text-center">Provider</p>
+					<div className="flex flex-row gap-2">
+						<button className="border p-2" onClick={() => provider.detach()}>
+							Detach
+						</button>
+						<button className="border p-2" onClick={() => provider.attach()}>
+							Attach
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CollaborationStatus;
