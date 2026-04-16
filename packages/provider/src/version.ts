9c7454c814fc6aabeb630846cd4763c7c9b347cb
Jan Thurau <jan@janthurau.de>
2026-04-16 21:39:57 +0200

    fix lint

--- packages/provider/src/version.ts
+++ packages/provider/src/version.ts
@@ -1,5 +1,6 @@
-// @ts-expect-error - __HOCUSPOCUS_VERSION__ is replaced at build time by rolldown
-// eslint-disable-next-line
-export const version: string = typeof __HOCUSPOCUS_VERSION__ !== 'undefined'
-	? __HOCUSPOCUS_VERSION__
-	: 'unknown';
+export const version: string =
+	// @ts-expect-error - __HOCUSPOCUS_VERSION__ is replaced at build time by rolldown
+	typeof __HOCUSPOCUS_VERSION__ !== "undefined"
+		? // @ts-expect-error - __HOCUSPOCUS_VERSION__ is replaced at build time by rolldown
+			__HOCUSPOCUS_VERSION__
+		: "unknown";
